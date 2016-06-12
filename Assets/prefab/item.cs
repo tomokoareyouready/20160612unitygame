900ed5207a709c6992ad6b4b8277581fae9486d2
tomoko matsuoka <tomoko.matsuoka@fove-inc.com>
2016-06-12 18:33:58 +0900

    initial commit

---
+++ Assets/prefab/item.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class item : MonoBehaviour
+{
+	// トリガーとの接触時に呼ばれるコールバック
+	void OnTriggerEnter (Collider hit)
+	{
+		// 接触対象はPlayerタグですか？
+		if (hit.CompareTag ("Player")) {
+
+			// このコンポーネントを持つGameObjectを破棄する
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/script/palyerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class palyerController : MonoBehaviour 
+{
+	public float speed = 10;
+	void FixedUpdate()
+	{
+		//入力をxとzに代入
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		//同一のGameObjectが持つ Rigidbodyコンポーネントを取得
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		//rididbodyのx軸（横）とz軸（奥）に力を加える
+		//rigidbody.AddForce(x, 0, z);
+
+		//xとyにspeedを掛ける
+		rigidbody.AddForce(x* speed ,0, z* speed);
+}
+}
